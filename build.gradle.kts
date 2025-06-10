33b63527236351345acd23ef4e9cd4ce65ce7b09
Kraptor123 <kraptor123@gmail.com>
2025-06-11 01:39:23 +0300

    duzenlemeler

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,11 +38,11 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "user/repo")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kraptor123/Cs-GizliKeyif")
     }
 
     android {
-        namespace = "com.example"
+        namespace = "com.kraptor"
 
         defaultConfig {
             minSdk = 21
