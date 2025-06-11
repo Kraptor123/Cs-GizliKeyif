9b7d02e4a09e3555e31404ad5f7c25d15eaf4e1c
Kraptor123 <kraptor123@gmail.com>
2025-06-11 04:59:30 +0300

    hentaizm

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kraptor123/Cs-GizliKeyif")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kraptor123/Cs-GizliKeyif/")
     }
 
     android {
