64c2735fcd2803cfac02c94722f6a607e7ee2e63
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-05-24 14:10:58 +0300

    Build fix 2

--- XXXParodyHD/src/main/kotlin/com/kraptor/XXXParodyHDPlugin.kt
+++ XXXParodyHD/src/main/kotlin/com/kraptor/XXXParodyHDPlugin.kt
@@ -6,6 +6,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
 import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.VidStack
 import com.lagradost.cloudstream3.extractors.Voe
 
 @CloudstreamPlugin
