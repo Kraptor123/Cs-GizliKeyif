9b7c2e7f7c141e6f7b42ad5fd74d35076573940d
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-06-28 17:22:39 +0300

    Motherless domain update

--- Motherless/src/main/kotlin/com/kraptor/Motherless.kt
+++ Motherless/src/main/kotlin/com/kraptor/Motherless.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 
 class Motherless : MainAPI() {
-    override var mainUrl              = "https://motherless.com"
+    override var mainUrl              = "https://motherless.xxx"
     override var name                 = "Motherless"
     override val hasMainPage          = true
     override var lang                 = "en"
