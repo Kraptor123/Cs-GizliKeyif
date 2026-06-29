72221cda21075d3dba918a1e3a7c15cbf7d67852
Kraptor123 <89366989+Kraptor123@users.noreply.github.com>
2026-06-29 07:23:38 +0300

    Update build.gradle.kts

--- Motherless/build.gradle.kts
+++ Motherless/build.gradle.kts
@@ -1,11 +1,11 @@
 // ! Bu araç @Kraptor123 tarafından | @Cs-GizliKeyif için yazılmıştır.
-version = 11
+version = 12
 
 cloudstream {
     authors     = listOf("kraptor")
     language    = "en"
     description = "Motherless.com is a moral free file host where anything legal is hosted forever. Motherless has a very large and active community where you can meet like minded individuals."
-    status  = 0 // will be 3 if unspecified
+    status  = 1 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://motherless.com/&size=16"
 }
