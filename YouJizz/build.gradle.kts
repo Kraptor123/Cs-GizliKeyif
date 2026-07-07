b839cf984b7c9ab564480e2a1fdcaabc0178cd61
kraptor123 <kraptor121@gmail.com>
2026-07-07 23:23:14 +0300

    YouJizz: New Extension

---
+++ YouJizz/build.gradle.kts
@@ -0,0 +1,19 @@
+// ! Bu araç @Kraptor123 tarafından | @Cs-GizliKeyif için yazılmıştır.
+version = 1
+
+cloudstream {
+    authors     = listOf("kraptor")
+    language    = "en"
+    description = "Youjizz Porn Tube! Free porn movies and sex videos on your desktop or mobile phone."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://www.google.com/s2/favicons?sz=64&domain=youjizz.com"
+}
