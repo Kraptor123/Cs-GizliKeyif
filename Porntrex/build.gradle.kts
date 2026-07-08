d4f7d007b2ac10cfb5d079e95f5592d7165b85f6
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-07-08 18:08:35 +0300

    New Extension: PornTrex

---
+++ Porntrex/build.gradle.kts
@@ -0,0 +1,14 @@
+// ! This Extension Made By @ByAyzen for GizliKeyif
+version = 1
+
+cloudstream {
+    authors     = listOf("ByAyzen")
+    language    = "en"
+    description = "Watch HD and 4k best porn videos on PornTrex. Get your daily dose of porn from our huge collection of free hd and 4k porn videos."
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+
+    val domainIcon = "porntrex.com"
+    iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=http://$domainIcon&size=128"
+
+}
