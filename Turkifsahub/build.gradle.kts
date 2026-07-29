51ee0421468d6f575b0a09de1eec11830a4a13a2
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-07-29 22:04:25 +0300

    Turkifsahub devre dışı

--- Turkifsahub/build.gradle.kts
+++ Turkifsahub/build.gradle.kts
@@ -1,11 +1,11 @@
 // ! Bu araç @Kraptor123 tarafından | @Cs-GizliKeyif için yazılmıştır.
-version = 6
+version = 7
 
 cloudstream {
     authors     = listOf("ByAyzen")
     language    = "tr"
     description = "Türk İfşa Hub üzerinde en güncel ve ücretsiz VIP Türk İfşa videolarına ücretsiz ulaşın."
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("NSFW")
     iconUrl = "https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://turkifsahub.com/&size=128"
 }
