105d304ba8af3a5db3a9b7382ab860b4ed87deab
ByAyzen <189399597+ByAyzen@users.noreply.github.com>
2026-08-26 20:56:23 +0300

    CamCaps Fix

--- CamCaps/src/main/kotlin/com/kraptor/CamCaps.kt
+++ CamCaps/src/main/kotlin/com/kraptor/CamCaps.kt
@@ -11,7 +11,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 
 
 class CamCaps : MainAPI() {
-    override var mainUrl = "https://camcaps.io"
+    override var mainUrl = "https://camcaps.tv"
     override var name = "CamCaps"
     override val hasMainPage = true
     override var lang = "en"
@@ -35,32 +35,42 @@ class CamCaps : MainAPI() {
         "$mainUrl/search/videos/snapchat" to "Snapchat",
     )
 
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = if (page == 1) {
-            app.get(request.data).document
-        } else {
-            app.get("${request.data}&page=$page").document
-        }
-        val home = document.select("article.thumb").mapNotNull { it.toMainPageResult() }
 
-        return newHomePageResponse(request.name, home)
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val url = if (page == 1) {
+            request.data
+        } else {
+            "${request.data}?page=$page"
+        }
+
+        val document = app.get(url).document
+        val home = document.select("div.thumbs > article.thumb").mapNotNull { it.toMainPageResult() }
+
+        return newHomePageResponse(
+            list = HomePageList(
+                name = request.name,
+                list = home,
+                isHorizontalImages = true
+            ),
+            hasNext = home.isNotEmpty()
+        )
     }
 
     private fun Element.toMainPageResult(): SearchResponse? {
-        val title = this.selectFirst("h3")?.text() ?: return null
-        val rating = this.selectFirst("span.like-icon")?.text() ?: return null
-        val href = fixUrlNull(this.selectFirst("a")?.attr("href")) ?: return null
-        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+        val image = this.selectFirst("figure img, img") ?: return null
+        val title = this.selectFirst("h3")?.text() ?: image.attr("title")
+        val href = fixUrlNull(this.selectFirst("a[href*=/video/]")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(image.attr("data-src").ifEmpty { image.attr("src") })
 
         return newMovieSearchResponse(
             title,
-            "$href-=-${posterUrl?.substringAfter("//")}",
+            href,
             TvType.NSFW
         ) {
             this.posterUrl = posterUrl
-            this.score = Score.from100(rating.replace("%", ""))
         }
     }
+
 
     override suspend fun search(query: String, page: Int): SearchResponseList {
         val document = app.get("${mainUrl}/search/videos/$query?page=$page").document
@@ -72,11 +82,9 @@ class CamCaps : MainAPI() {
     override suspend fun quickSearch(query: String): List<SearchResponse>? = search(query)
 
     override suspend fun load(url: String): LoadResponse? {
-        val split = url.split("-=-")
-        val document = app.get(split[0]).document
-
+        val document = app.get(url).document
         val title = document.selectFirst("h1")?.text()?.trim() ?: return null
-        val poster = "https://${split[1]}"
+        val poster = document.selectFirst("meta[property=og:image]")?.attr("content")
         val description = document.selectFirst("article.about p")?.text()?.trim()
         val year = document.selectFirst("div.extra span.C a")?.text()?.trim()?.toIntOrNull()
         val tags = document.select("div.group a").map { it.text() }
@@ -84,7 +92,7 @@ class CamCaps : MainAPI() {
         val duration = document.selectFirst("span.runtime")?.text()?.split(" ")?.first()?.trim()?.toIntOrNull()
         val recommendations = document.select("article.thumb").mapNotNull { it.toMainPageResult() }
         val actors = document.select("span.valor a").map { Actor(it.text()) }
-        val trailer = Regex("""embed\/(.*)\?rel""").find(document.html())?.groupValues?.get(1)
+        val trailer = Regex("""embed/(.*)\?rel""").find(document.html())?.groupValues?.get(1)
             ?.let { "https://www.youtube.com/embed/$it" }
 
        return newMovieLoadResponse(title, url, TvType.NSFW, url) {
